2cbf73971028ea5edea5a57ee7182e4cf322ec6d
Russell Elliott <russell@quadsquad.net>
2024-09-18 17:47:24 -0700

    maybe hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import IndexPage from './pages/indexPage';
 import ProjectPage from './pages/projectPage';
 
